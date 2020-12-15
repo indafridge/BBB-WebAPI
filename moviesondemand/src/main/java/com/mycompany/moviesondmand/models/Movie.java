94a2a5eaf03b2bffc436352544c43d6288168026
Mindaugas <36074178+MindaugasPrismantas@users.noreply.github.com>
2020-12-15 15:57:53 +0000

    Update Movie.java

--- moviesondemand/src/main/java/com/mycompany/moviesondmand/models/Movie.java
+++ moviesondemand/src/main/java/com/mycompany/moviesondmand/models/Movie.java
@@ -10,6 +10,7 @@ package com.mycompany.moviesondmand.models;
  * @author mynde
  */
 public class Movie {
+    private int id;
     private String name;
     private String summary;
     private boolean watched;
@@ -19,11 +20,20 @@ public class Movie {
         
     }
     
-    public Movie(String name, String summary, boolean watched, boolean recommended){
+    public Movie(int id, String name, String summary, boolean watched, boolean recommended){
+        this.id = id;
         this.name = name;
         this.summary = summary;
         this.watched = false;
         this.recommended = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
